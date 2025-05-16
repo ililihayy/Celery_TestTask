98611955d4dfb34c27abf9e3c6bc6837265beec1
ililihayy <lilipushkar15@gmail.com>
2025-05-16 23:41:15 +0300

    refactor: move API to config

--- app/config.py
+++ app/config.py
@@ -4,3 +4,7 @@ MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017")
 MONGO_DB_NAME = "user_data"
 
 CELERY_BROKER = os.getenv("CELERY_BROKER", "amqp://guest:guest@rabbitmq:5672//")
+
+API_USERS = "https://jsonplaceholder.typicode.com/users"
+API_ADDRESSES = "https://random-data-api.com/api/address/random_address"
+API_CREDIT_CARDS = "https://random-data-api.com/api/v2/credit_cards"
